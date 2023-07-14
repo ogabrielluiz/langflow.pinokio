6dc7e06645ac5a985ab79d90f9eab1852b2ab9a0
cocktailpeanut <cocktailpeanuts@proton.me>
2023-07-14 09:01:32 -0400

    Prioritize install just to emphasize for first time users

--- pinokio.js
+++ pinokio.js
@@ -1,12 +1,12 @@
 module.exports = {
   menu: [
     {
-      html: "<i class='fa-solid fa-play'></i> Launch",
-      href: "launch.json",
-    },
-    {
       html: "<i class='fa-solid fa-gear'></i> Install",
       href: "start.json",
+    },
+    {
+      html: "<i class='fa-solid fa-play'></i> Launch",
+      href: "launch.json",
     },
   ],
 };
